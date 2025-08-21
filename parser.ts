623c53680f89eb48cc04464b8adb5560266db397
Tachera Sasi <tacherasasi@gmail.com>
2025-08-21 14:43:13 +0300

    Add constructor and placeholder methods to Parser class

--- parser.ts
+++ parser.ts
@@ -1,6 +1,19 @@
+import type { Lexer } from "./lexer";
 import type { Token } from "./tokens";
 
 export class Parser{
     private tokens: Token[] = [];
     private pos: number = 0;
+
+    constructor(lexer: Lexer){
+        this.tokenizeAll()
+    }
+
+    private tokenizeAll(){}
+
+    private nextToken(): Token | null {}
+
+    parseAndExecute(){
+        
+    }
 }
