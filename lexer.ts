89291cd8dfb56785d08126e87df2b91a9a63f792
Tachera Sasi <tacherasasi@gmail.com>
2025-08-21 11:47:02 +0300

    Implement Lexer class with position tracking and input handling

---
+++ lexer.ts
@@ -0,0 +1,13 @@
+export class Lexer {
+  private pos: number = 0;
+  private current: string | null = null;
+
+  constructor(private input: string) {
+    this.current = input[0] || null;
+  }
+
+  private advance() {
+    this.pos++;
+    this.current = this.pos < this.input.length ? this.input[this.pos] ?? null : null;
+  }
+}
