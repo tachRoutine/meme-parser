dbb5b0acb63a1f8709db894effa81b09f07107ab
Tachera Sasi <tacherasasi@gmail.com>
2025-08-21 15:11:33 +0300

    Refactor main function to improve input handling and logging

--- main.ts
+++ main.ts
@@ -2,15 +2,16 @@ import { Lexer } from "./lexer";
 import { Parser } from "./parser";
 
 async function main() {
-  let input = Bun.file('main.meme').text();
-  console.log("INPUT ",input)
+  const fileData = await Bun.file("main.meme").text();
+  const input: String = await fileData;
+  console.log("INPUT ", input);
 
   try {
     const lexer = new Lexer((await input).trim());
     const parser = new Parser(lexer);
     parser.parseAndExecute();
   } catch (err: any) {
-    console.error('Error:', err.message);
+    console.error("Error:", err.message);
     process.exit(1);
   }
 }
