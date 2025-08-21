9f8bd491b7dd2aa5b778129151cd7917ebe72a10
Tachera Sasi <tacherasasi@gmail.com>
2025-08-21 14:47:16 +0300

    Refactor Parser class for improved formatting and consistency

--- parser.ts
+++ parser.ts
@@ -1,25 +1,31 @@
 import type { Lexer } from "./lexer";
 import { TokenType, type Token } from "./tokens";
 
-export class Parser{
-    private tokens: Token[] = [];
-    private pos: number = 0;
+export class Parser {
+  private tokens: Token[] = [];
+  private pos: number = 0;
 
-    constructor(private lexer: Lexer){
-        this.tokenizeAll()
+  constructor(private lexer: Lexer) {
+    this.tokenizeAll();
+  }
+
+  private tokenizeAll(): void {
+    let token: Token | null;
+    do {
+      token = this.lexer.nextToken();
+      this.tokens.push(token);
+    } while (token.type !== TokenType.EOF);
+  }
+
+  private nextToken(): Token {
+    if (this.pos < this.tokens.length) {
+      return this.tokens[this.pos++] ?? { type: TokenType.EOF, value: "" }; //TODO: Will change this to a type error in the future
     }
+    return { type: TokenType.EOF, value: "" };
+  }
 
-    private tokenizeAll(): void{
-        let token: Token | null;
-        do{
-            token = this.lexer.nextToken();
-            this.tokens.push(token);
-        }while (token.type !== TokenType.EOF)
-    }
-
-    private nextToken(): Token | null {}
-
-    parseAndExecute(){
-
-    }
+  parseAndExecute() {
+    const messageParts: string[] = [];
+    let token = this.nextToken();
+  }
 }
